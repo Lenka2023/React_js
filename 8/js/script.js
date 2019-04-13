308c8c7a088ef886a999098b88183a724a85f9ab
Lenka2023 <lenka20231@gmail.com>
2019-04-13 23:29:24 +0300

    8

---
+++ 8/js/script.js
@@ -0,0 +1,13 @@
+//console.log("123");
+
+    
+var Block = React.createClass ({
+  render:function () {
+    return (<h3> Lena </h3>);
+  }
+});
+
+      ReactDOM.render(
+        <Block/>,
+        document.getElementById("example")
+      );
