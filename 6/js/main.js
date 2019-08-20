515c36004a39690bbd7ec98cfc40f303ef15a423
Lenka2023 <lenka20231@gmail.com>
2019-08-20 13:48:28 +0300

    have done 6

---
+++ 6/js/main.js
@@ -0,0 +1,26 @@
+var Task=React.createClass({
+  edit:function{
+    alert("редактировать");
+  },
+  remove:function{
+    alert("удалить");
+  },
+  render: function(){
+
+  return(
+  <div className="box">
+    <div className="text">{this.props.children}</div>
+  <button onClick={this.edit} > Редактировать</button>
+<button onClick={this.remove} > Удалить</button>
+</div>
+  );
+}
+});
+const app=document.getElementById("program");
+ReactDOM.render(
+<div >
+  <Task>Task number 1</Task>
+  <Task>Task number 2</Task>
+  <Task>Task number 3</Task>
+</div>,
+ document.getElementById("program"));
